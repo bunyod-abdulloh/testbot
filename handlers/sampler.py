b613141fb64d903c837ad513e8875f34b1990cd0
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-05-02 12:28:51 +0500

    Updated

--- handlers/sampler.py
+++ handlers/sampler.py
@@ -1,13 +1,54 @@
-from pypdf import PdfReader
-from pypdf.generic import EncodedStreamObject
+import PyPDF2
 
-# Create a PDF reader object
-reader = PdfReader('Test Master.pdf')
 
-# Get the number of pages in the PDF
-print(f"Number of pages: {len(reader.pages)}")
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, "rb") as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        # for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[7]
+        text += page.extract_text()
+    return text
 
-# Extract text from the first page
-page = reader.pages[7]
-print(page.extract_text())
+
+test = []
+
+pdf_path = "Test Master.pdf"
+extracted_text = extract_text_from_pdf(pdf_path)
+test.append(extracted_text)
+
+all_questions_data = test[0].split('\n\n')
+a_2 = all_questions_data[0].split("\n")
+print(a_2)
+# print(all_questions_data)
+questions = []
+# a_zero = []
+# a_str = str()
+# all_questions_data = test[0].split('\n\n')
+# questions = []
+# c = 0
+# for question_data in all_questions_data:
+#     c += 1
+#     print(c)
+#     print(question_data)
+    # questions.append(question_data.split('\n')[0])
+# print(questions)
+
+
+for index, question_data in enumerate(all_questions_data):
+
+    start_index = question_data.index(f'1.')
+    end_index = question_data.index(f'\nA)')
+#         a_one = question_data.index('A)')
+#         a_two = question_data.index('B)')
+#         a_zero.append(question_data[a_one:a_two])
+#         a_str = a_zero[0].split('A) ')[1]
+    print(end_index)
+    # for n in range(45):
+    questions.append(question_data[start_index:end_index])
+print(questions)
+#
+# print(questions)
+# print(a_str)
+# print(len(all_questions_data))
 
